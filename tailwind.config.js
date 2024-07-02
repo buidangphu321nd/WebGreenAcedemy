92a8c3eee7f89a6501323aeb2b7a980e76afda93
Khanhnguyen1309 <khanhnguyen130901@gmail.com>
2024-07-02 15:30:35 +0700

    Khanh update Some count that matters và Clients Testimonial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
