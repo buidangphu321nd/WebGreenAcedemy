0d6964bc31ecb3bb175a958564d3c8b5c862de73
VanTuan2609 <vantuan191601080@gmail.com>
2024-07-03 09:06:44 +0700

    final code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      fontFamily: {
-        jakarta: ["Plus Jakarta Sans", "sans-serif"],
-        playfair: ["Playfair Display", "serif"],
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      boxShadow: {
-        custom: "0px 39px 100px 0px #193E6C1F",
-=======
-      fontFamily:{
-        playfair: ['Playfair Display', 'serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
->>>>>>> VanPhuc
-=======
-      fontFamily: {
-        playfair: ['Playfair Display', 'serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
->>>>>>> NgocKhanh
-      },
-    },
+    extend: {},
+    plugins: [],
   },
-  plugins: [],
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> NgocKhanh
